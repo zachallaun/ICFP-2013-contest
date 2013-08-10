0f53ed43a63ffa007bd5be4a197c4697af6a7e80
Kevin J. Lynagh <kevin@keminglabs.com>
2013-08-10 11:47:28 -0600

    Tidy up size and op tests.

--- test/com/hackerschool/icfp_contest_2013/t_program_gen.clj
+++ test/com/hackerschool/icfp_contest_2013/t_program_gen.clj
@@ -2,16 +2,18 @@
   (:require [midje.sweet :refer :all]
             [com.hackerschool.icfp-contest-2013.program-gen :refer :all]))
 
-(def sizeop (juxt size op))
-
 (def program-examples
-  [[0  1 #{}]
-   ['x 1 #{}]
-   ['(if0 0 0 0) 4 #{'if0}]
-   ['(if0 (if0 0 0 0) 0 0) 7 #{'if0}]
-   ['(someop 0) 2 #{'someop}]
-   ['(fold (op1 0) 1 (lambda (_ _) (op2 1 0))) 8 #{'fold 'op1 'op2}]])
+  "Vector of known valid [program size opset] triples."
+  '[[0 1 #{}]
+    [x 1 #{}]
+    [(if0 0 0 0) 4 #{if0}]
+    [(if0 (if0 0 0 0) 0 0) 7 #{if0}]
+    [(someop 0) 2 #{someop}]
+    [(fold (op1 0) 1 (lambda (_ _) (op2 1 0))) 8 #{fold op1 op2}]])
 
-(facts "about op"
+(def sizeop
+  (juxt size op))
+
+(facts "Size and operator set of example programs are calculated correctly by `size` and `op`."
   (doseq [[p size opset] program-examples]
     (sizeop p) => [size opset]))
