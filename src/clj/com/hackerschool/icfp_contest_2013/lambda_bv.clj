fee47118ac0687f58f04c247f8382569d0d0222b
Lindsey Kuper <lindsey@composition.al>
2013-08-10 22:46:25 -0400

    More bitshifting stuff

--- src/clj/com/hackerschool/icfp_contest_2013/lambda_bv.clj
+++ src/clj/com/hackerschool/icfp_contest_2013/lambda_bv.clj
@@ -119,17 +119,52 @@
 ;; todo: "shl1" | "shr1" | "shr4" | "shr16"
 ;; todo: "and" | "or" | "xor" | "plus"
 
-;; assumes that input is a list.
+;; assumes that input is a vector.
 ;; TODO: test me
 (defn bitvector-shl1o [bv out]
   (fresh [fstbit rstbits]
     (conso fstbit rstbits bv)
-    (appendo rstbits 0 out)))
+    (appendo rstbits [0] out)))
+
+;; proof of concept.  you could also write this recursively, maybe?
+(defn bitvector-shl4o [bv out]
+  (fresh [tmp1 tmp2 tmp3]
+    (bitvector-shl1o bv tmp1)
+    (bitvector-shl1o tmp1 tmp2)
+    (bitvector-shl1o tmp2 tmp3)
+    (bitvector-shl1o tmp3 out)))
 
 (defn bitvector-shr1o [bv out]
   (fresh [fstbv lastbit]
     (appendo fstbv lastbit bv)
     (conso 0 fstbv out)))
+
+
+
+;; turn [b1 b2 b3 b4 ... bn] into [0 0 0 0 b1 b2 b3 b4 ... bn-3]
+(defn bitvector-shr4o [bv out]
+  (fresh [b1 b2 b3 b4 middlebits bn3 bn2 bn1 bn]
+     (== [b1 b2 b3 b4 middlebits] bv)
+     
+     ;; ensure that all the bits are bits!
+     (bito b1) (bito b2) (bito b3) (bito b4)
+     (bito bn3) (bito bn2) (bito bn1) (bito bn)
+
+     (== [0 0 0 0 b1 b2 b3 b4 middlebits] out)))
+
+(comment
+  (run 1 [q] (bito 1))
+
+  (run 1 [q] (bitvector-shl1o [1 1 1] q))
+
+  (run 1 [q] (bitvector-shl4o [1 1 1 1 1] q))
+
+  (run 1 [q] (bitvector-shr4o [1 1 1 1 1 1 1 1] q))
+
+)
+     
+     
+     
 
 (defn run-bodyo [e env out]
   (conde
